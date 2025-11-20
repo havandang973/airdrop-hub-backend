fb5341d052500325f1884e166477f903f8629b83
Ha Van <121249132+havandang973@users.noreply.github.com>
2025-11-20 10:45:10 +0700

    add coin

--- src/market/api/market.controller.ts
+++ src/market/api/market.controller.ts
@@ -18,12 +18,12 @@ export class MarketController {
 
     @Get('/price/binance')
     @UseGuards(ApiKeyGuard)
-    async getPriceBinance(@Query() queryParams: any) {
+    async getPriceBinance() {
         const response = await fetch("https://api.binance.com/api/v3/ticker/price");
         const data = await response.json();
-        const query = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'ADAUSDT', 'SOLUSDT', 'DOGEUSDT', 'DOTUSDT', 'POLUSDT', 'LTCUSDT'];
+        // const query = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'ADAUSDT', 'SOLUSDT', 'DOGEUSDT', 'DOTUSDT', 'POLUSDT', 'LTCUSDT'];
         const filteredData = data.filter((item: any) => {
-            return item.symbol.endsWith('USDT') && Number(item.price) && query.includes(item.symbol);
+            return item.symbol.endsWith('USDT') && Number(item.price); //&& query.includes(item.symbol);
         })
             .sort((a: any, b: any) => Number(b.price) - Number(a.price))
         // .reduce((acc: any, item: any) => {
@@ -39,9 +39,9 @@ export class MarketController {
     async getPriceBybit() {
         const response = await fetch("https://api.bybit.com/v5/market/tickers?category=spot");
         const data = await response.json();
-        const query = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'ADAUSDT', 'SOLUSDT', 'DOGEUSDT', 'DOTUSDT', 'POLUSDT', 'LTCUSDT'];
+        // const query = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'ADAUSDT', 'SOLUSDT', 'DOGEUSDT', 'DOTUSDT', 'POLUSDT', 'LTCUSDT'];
         const filteredData = data.result.list.filter((item: any) => {
-            return item.symbol.endsWith('USDT') && Number(item.usdIndexPrice) && query.includes(item.symbol);
+            return item.symbol.endsWith('USDT') && Number(item.usdIndexPrice); //&& query.includes(item.symbol);
         })
             .sort((a: any, b: any) => Number(b.usdIndexPrice) - Number(a.usdIndexPrice))
             .map((item: any) => ({
@@ -57,10 +57,10 @@ export class MarketController {
     async getPriceKucoin() {
         const response = await fetch("https://api.kucoin.com/api/v1/market/allTickers");
         const data = await response.json();
-        const query = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'ADAUSDT', 'SOLUSDT', 'DOGEUSDT', 'DOTUSDT', 'POLUSDT', 'LTCUSDT'];
+        // const query = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'ADAUSDT', 'SOLUSDT', 'DOGEUSDT', 'DOTUSDT', 'POLUSDT', 'LTCUSDT'];
         const filteredData = data.data.ticker.filter((item: any) => {
             const symbol = item.symbol.replace('-', '');
-            return item.symbol.endsWith('USDT') && Number(item.last) && query.includes(symbol);
+            return item.symbol.endsWith('USDT') && Number(item.last); //&& query.includes(symbol);
         })
             .sort((a: any, b: any) => Number(b.last) - Number(a.last))
             .map((item: any) => ({
@@ -75,9 +75,9 @@ export class MarketController {
     async getPriceHuobi() {
         const response = await fetch("https://api.huobi.pro/market/tickers");
         const data = await response.json();
-        const query = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'ADAUSDT', 'SOLUSDT', 'DOGEUSDT', 'DOTUSDT', 'POLUSDT', 'LTCUSDT'];
+        // const query = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'ADAUSDT', 'SOLUSDT', 'DOGEUSDT', 'DOTUSDT', 'POLUSDT', 'LTCUSDT'];
         const filteredData = data.data.filter((item: any) => {
-            return item.symbol.toUpperCase().endsWith('USDT') && Number(item.close) && query.includes(item.symbol.toUpperCase());
+            return item.symbol.toUpperCase().endsWith('USDT') && Number(item.close); // && query.includes(item.symbol.toUpperCase());
         })
             .sort((a: any, b: any) => Number(b.close) - Number(a.close))
             .map((item: any) => ({
@@ -93,10 +93,10 @@ export class MarketController {
     async getPriceOkx() {
         const response = await fetch("https://www.okx.com/api/v5/market/tickers?instType=SPOT");
         const data = await response.json();
-        const query = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'ADAUSDT', 'SOLUSDT', 'DOGEUSDT', 'DOTUSDT', 'POLUSDT', 'LTCUSDT'];
+        // const query = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'ADAUSDT', 'SOLUSDT', 'DOGEUSDT', 'DOTUSDT', 'POLUSDT', 'LTCUSDT'];
         const filteredData = data.data.filter((item: any) => {
             const symbol = item.instId.replace('-', '');
-            return item.instId.endsWith('USDT') && Number(item.last) && query.includes(symbol);
+            return item.instId.endsWith('USDT') && Number(item.last) //&& query.includes(symbol);
         })
             .sort((a: any, b: any) => Number(b.last) - Number(a.last))
             .map((item: any) => ({
